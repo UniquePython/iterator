f343419ba1cab46af211d48201d8378d287dc3ea
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-07-08 21:50:06 +0530

    Get a basic example going

---
+++ examples/main.c
@@ -0,0 +1,29 @@
+#include "array_iterator.h"
+#include "iterator.h"
+
+#include <stdio.h>
+
+void printInt(void *elem)
+{
+    printf("%d\n", *(int *)elem);
+}
+
+void printFloat(void *elem)
+{
+    printf("%g\n", *(float *)elem);
+}
+
+int main(void)
+{
+    int intArr[] = {10, 20, 30, 40};
+    Iterator intIt = newArrayIterator(sizeof(int), intArr, sizeof intArr / sizeof *intArr);
+    printAll(intIt, sizeof(int), printInt);
+    IteratorDestroy(&intIt);
+
+    float floatArr[] = {10.5f, 20.5f, 30.5f, 40.5f};
+    Iterator floatIt = newArrayIterator(sizeof(float), floatArr, sizeof floatArr / sizeof *floatArr);
+    printAll(floatIt, sizeof(float), printFloat);
+    IteratorDestroy(&floatIt);
+
+    return 0;
+}
