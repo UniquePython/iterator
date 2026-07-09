a6183a5f7afab54b81216fdecdf8474d937b9f03
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-07-09 16:02:49 +0530

    Add Chain Iterator

---
+++ include/chain_iterator.h
@@ -0,0 +1,15 @@
+#ifndef CHAIN_ITERATOR_H_
+#define CHAIN_ITERATOR_H_
+
+#include "iterator.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct chain_iterator_t ChainIterator;
+
+bool ChainIteratorNext(void *state, void *out);
+void ChainIteratorDestroy(void *state);
+Iterator newChainIterator(size_t inElemSize, Iterator innerA, Iterator innerB);
+
+#endif
---
+++ src/chain_iterator.c
@@ -0,0 +1,64 @@
+#include "chain_iterator.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+struct chain_iterator_t
+{
+    size_t elemSize;
+    void *buffer;
+    Iterator innerA;
+    Iterator innerB;
+};
+
+bool ChainIteratorNext(void *state, void *out)
+{
+    ChainIterator *chainIt = (ChainIterator *)state;
+
+    if (!out)
+        return false;
+
+    if (!chainIt->innerA.next(chainIt->innerA.state, chainIt->buffer))
+        if (!chainIt->innerB.next(chainIt->innerB.state, chainIt->buffer))
+            return false;
+
+    memcpy(out, chainIt->buffer, chainIt->elemSize);
+
+    return true;
+}
+
+void ChainIteratorDestroy(void *state)
+{
+    ChainIterator *chainIt = (ChainIterator *)state;
+    free(chainIt->buffer);
+    free(chainIt);
+}
+
+Iterator newChainIterator(size_t inElemSize, Iterator innerA, Iterator innerB)
+{
+    Iterator iterator = {0};
+
+    ChainIterator *chainIterator = malloc(sizeof *chainIterator);
+
+    if (!chainIterator)
+        return iterator;
+
+    void *buffer = malloc(inElemSize);
+
+    if (!buffer)
+    {
+        free(chainIterator);
+        return iterator;
+    }
+
+    chainIterator->elemSize = inElemSize;
+    chainIterator->buffer = buffer;
+    chainIterator->innerA = innerA;
+    chainIterator->innerB = innerB;
+
+    iterator.state = chainIterator;
+    iterator.next = ChainIteratorNext;
+    iterator.destroy = ChainIteratorDestroy;
+
+    return iterator;
+}
